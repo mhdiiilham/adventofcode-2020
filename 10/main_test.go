89880903aca41782c95b68b80335892679a80e6d
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-12-10 20:34:59 +0700

    fix(10/main_test.go): test case if file does not exist

--- 10/main_test.go
+++ 10/main_test.go
@@ -10,3 +10,8 @@ func TestReadFromFile(t *testing.T) {
 	input := readFromFile("test.txt")
 	assert.Equal(t, 31, len(input), "Length of test input should be equal to 31")
 }
+
+func TestReadFromFileFail(t *testing.T) {
+	input := readFromFile("tett.txt")
+	assert.Equal(t, 0, len(input), "Length of tett.txt input should be equal to 0")
+}
