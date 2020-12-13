afb91c7d1bb97acb0f4c8812774cec27438e8e00
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-12-13 15:50:10 +0700

    test(13/main_test.go): create test of function multiplyingTime

--- 13/main_test.go
+++ 13/main_test.go
@@ -17,3 +17,10 @@ func TestReadInputSuccess(t *testing.T) {
 	assert.Equal(t, "939", input[0], "Input index 0 should be equal to 939")
 	assert.Equal(t, "7,13,x,x,59,x,31,19", input[1], "Input index 1 should be equal to 7,13,x,x,59,x,31,19")
 }
+
+func TestMultipliedDepartAndWait(t *testing.T) {
+	busID := 59
+	minDepartTime := 939
+	multipliedDepartAndWaitTime := multiplyingTime(busID, minDepartTime)
+	assert.Equal(t, 295, multipliedDepartAndWaitTime, "The multiplied of minutes waiting and bus id should be equal to 295")
+}
